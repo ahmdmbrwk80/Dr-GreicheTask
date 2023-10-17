a4f62d7c9651499921211f82011b36a02ea529d1
Ahmed Mabrouk <dell@DESKTOP-5SJF271>
2023-10-17 16:04:23 +0300

    Add project files.

---
+++ Dr-GreicheTask.DAL/Migrations/20231017120347_init4.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dr_GreicheTask.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class init4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+     name: "IX_InsurancePapers_EmployeeId",
+     table: "InsurancePapers");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
